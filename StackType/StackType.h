63bac649e2c2feb9db5a39b7c0c4e77c0de4e398
Brishty <52675761+TamannaAhsan@users.noreply.github.com>
2019-07-23 14:57:56 +0600

    Add files via upload

---
+++ StackType/StackType.h
@@ -0,0 +1,23 @@
+#ifndef STACKTYPE_H_INCLUDED
+#define STACKTYPE_H_INCLUDED
+
+const int MAX_ITEMS=5;
+class FullStack{};
+class EmptyStack{};
+
+template<class ItemType>
+class StackType{
+public:
+    StackType();
+    bool IsFull();
+    bool IsEmpty();
+    void Push(ItemType);
+    void Pop();
+    ItemType Top();
+private:
+    int top;
+    ItemType item[MAX_ITEMS];
+};
+
+
+#endif // STACKTYPE_H_INCLUDED
---
+++ StackType/main.cpp
@@ -0,0 +1,10 @@
+#include"StackType.h"
+#include"StackType.cpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    StackType<int>s1;
+
+}
