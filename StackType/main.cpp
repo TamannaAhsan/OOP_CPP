4c4f68314d992e62fd7cf4ba7dc9ed55ea5002b7
Brishty <52675761+TamannaAhsan@users.noreply.github.com>
2019-07-24 23:15:37 +0600

    Update main.cpp

--- StackType/main.cpp
+++ StackType/main.cpp
@@ -5,6 +5,85 @@ using namespace std;
 
 int main()
 {
-    StackType<int>s1;
+    
+    StackType<int>myStack;
+    if(myStack.IsEmpty()){
+        cout<<"Stack is Empty"<<endl;
+    }
 
+    myStack.Push(5);
+    myStack.Push(7);
+    myStack.Push(4);
+    myStack.Push(2);
+     if(myStack.IsEmpty()){
+        cout<<"Stack is Empty"<<endl;
+    }
+    else{
+        cout<<"Stack is not Empty"<<endl;
+    }
+
+    if(myStack.IsFull()){
+        cout<<"Stack is Full"<<endl;
+    }
+    else{
+        cout<<"Stack is not Full"<<endl;
+    }
+    StackType<int>myTempStack1=myStack;
+    StackType<int>myTempStack2;
+
+    for(int i=0; i<4; i++){
+        myTempStack2.Push(myTempStack1.Top());
+        myTempStack1.Pop();
+    }
+    for(int i=0; i<4; i++){
+        cout<<myTempStack2.Top()<<" ";
+        myTempStack2.Pop();
+    }
+    cout<<endl;
+    myStack.Push(3);
+    myTempStack1=myStack;
+        for(int i=0; i<5; i++){
+        myTempStack2.Push(myTempStack1.Top());
+        myTempStack1.Pop();
+    }
+    for(int i=0; i<5; i++){
+        cout<<myTempStack2.Top()<<" ";
+        myTempStack2.Pop();
+    }
+    cout<<endl;
+     if(myStack.IsFull()){
+        cout<<"Stack is Full"<<endl;
+    }
+    else{
+        cout<<"Stack is not Full"<<endl;
+    }
+    myStack.Pop();
+    myStack.Pop();
+    cout<<myStack.Top()<<endl;
+
+    string a;
+    cin>>a;
+    cout<<a<<endl;
+
+    StackType<char>charStack;
+    for(int i=0; i<a.size(); i++){
+        if(charStack.IsEmpty())
+            charStack.Push(a[i]);
+        else if(charStack.Top()=='(' && a[i]==')')
+                charStack.Pop();
+        else if(charStack.Top()=='(' && a[i]=='(')
+            charStack.Push(a[i]);
+
+        else if(charStack.Top()==')' && a[i]=='(')
+            charStack.Pop();
+        else if(charStack.Top()==')' && a[i]==')')
+            charStack.Push(a[i]);
+
+    }
+    if(charStack.IsEmpty()){
+        cout<<"Balanced"<<endl;
+    }
+    else{
+        cout<<"NotBalanced"<<endl;
+    }
 }
